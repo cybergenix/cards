e92ade6ffa70eae0f1aefe7dea06589cd28f6f5f
Mattan Griffel <mattangriffel@gmail.com>
2014-07-26 12:31:33 -0400

    Fix flashes and a few other design fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if user_signed_in?
       @cards = current_user.card_backlog
+    else
+      render layout: 'home'
     end
   end
 end
